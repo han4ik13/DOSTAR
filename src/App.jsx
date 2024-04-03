ce434067729da9b2fc8fad1912355b452d5342ed
han4ik13 <147221593+han4ik13@users.noreply.github.com>
2024-04-04 02:17:31 +0600

    fix problem with img

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Footer } from './components/footer/Footer'
 import { Header } from './components/header/Header'
 import { Home } from './pages/Home'
 import { About } from './pages/About'
+import { Menu } from './pages/Menu'
 import { Delivery } from './pages/Delivery'
 
 import ScrollToTop from './utils/scrollToTop'
@@ -20,6 +21,7 @@ function App() {
 
 					<Route path='/' element= {<Home/>} />
 					<Route path='/About' element={<About />} /> 
+					<Route path='/Menu' element={<Menu />} /> 
 					<Route path='/Delivery' element={<Delivery />} /> 
 
 				</Routes>
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,10 @@
+export const Menu = () => {
+
+	return (
+		<section className="about-section">
+			<div className="container">
+			<img className="photo" src='' alt="drive image" />
+			</div>
+		</section>
+	)
+}
